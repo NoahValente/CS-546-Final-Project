d433b0907d4409dc71d2701c997bf1eef8f0957a
Hasumi Tanemori <hasumi@tanemori.org>
2022-08-12 09:59:32 -0400

    Fixed signup and combined forms in html

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,16 @@
 (function () {
 
-    $(document).ready(function errorHandeling(){
-        let error = $('#error');
-        error.hide(); 
-        
-    }
-)} ) ();
+    $(document).ready(function() {
+        // On refresh check if there are values selected
+        if (localStorage.userType) {
+                // Select the value stored
+            $('#userType').val( localStorage.userType );
+        }
+    });
+    
+    // On change store the value
+    $('#userType').on('change', function(){
+        var currentVal = $(this).val();
+        localStorage.setItem('userType', currentVal );
+    });
+} ) ();
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,29 +1,54 @@
 // there will be a dropdown on sign-up page that changes between user/business sign-up forms
 // upon dropdown change, the page will render different questions needed to sign-up for different accounts
 
-// document.getElementById(idthing).value === user
-// hide the business form
-// show the user form
-
-// document.getElementById(idthing).value === business
-// show the business form
-// hide the user form
-
-(function () {
+(function ($) {
     const dropdown =  document.getElementById('userType');
-    const userForm = document.getElementById('userSignup');
-    const businessForm = document.getElementById('businessSignup');
+    const form = document.getElementById('signupForm');
+    const heading = document.getElementById('formHeading');
+    const userFields = document.getElementsByClassName('userFields');
+    const businessFields = document.getElementsByClassName('businessFields');
     dropdown.addEventListener('change', function() {
         if (this.value === "User") {
-            userForm.hidden = false;
-            businessForm.hidden = true;
+            form.hidden = false;
+            form.setAttribute("action", "/signup/user");
+            heading.innerHTML = "User Sign Up";
+            for(field of userFields) {
+                field.hidden = false;
+                field.disabled = false;
+            }
+            for(field of businessFields) {
+                field.hidden = true;
+                field.disabled = true;
+            }
         } else if (this.value === "Business") {
-            userForm.hidden = true;
-            businessForm.hidden = false;
+            form.hidden =false;
+            form.setAttribute("action", "/signup/business");
+            heading.innerHTML = "Business Sign Up";
+            for(field of userFields) {
+                field.hidden = true;
+                field.disabled = true;
+            }
+            for(field of businessFields) {
+                field.hidden = false;
+                field.disabled = false;
+            }
         } else {
-            userForm.hidden = true;
-            businessForm.hidden = true;
+            form.hidden = true;
+        }
+    })
+    $(document).ready(function() {
+        // On refresh check if there are values selected
+        if (localStorage.userType) {
+                // Select the value stored
+            $('#userType').val( localStorage.userType );
         }
     });
-  }) ();
+    
+    // On change store the value
+    $('#userType').on('change', function(){
+        var currentVal = $(this).val();
+        localStorage.setItem('userType', currentVal );
+    });
+    
+})(window.jQuery);
   
