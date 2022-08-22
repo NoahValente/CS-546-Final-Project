07d13f5b953b45060ddfdd936a118ed949c81796
cavingada <cavingada@gmail.com>
2022-08-22 07:58:08 -0400

    Revert "Client side validation for signup form"

--- public/js/post.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    //var postImage = $('#postImage');
-    var uploaded_image = "";
-
-    $('#postImage').on('change', function(){
-        console.log($('#postImage').val());
-    })
-})(window.jQuery);
